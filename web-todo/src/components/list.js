8dcd0881e290389cabfe1de2c976e61f3c17ff44
Debdut Karmakar <iawaiponly@gmail.com>
2021-04-04 00:46:46 +0530

    list todos

--- web-todo/src/components/list.js
+++ web-todo/src/components/list.js
@@ -1,3 +1,17 @@
-export default function List () {
-  return <></>
+export default function List ({ todos = [] }) {
+  return (
+    <ul class='p-0 md:p-6 space-y-4'>
+      { todos.map((todo, index) => <Todo todo={todo} />) }
+    </ul>
+  )
+}
+
+function Todo ({ todo }) {
+  return (
+    <li>
+      {todo.link ? 
+        <a href={todo.link} class='border-b' target='_blank' rel='noopener noreferrer'>{todo.title}</a> :
+        todo.title }
+    </li>
+  )
 }
--- web-todo/src/index.js
+++ web-todo/src/index.js
@@ -4,6 +4,8 @@ import Nav from './components/nav'
 import Finder from './components/finder'
 import List from './components/list'
 
+import Todos from './store/todo'
+
 import './style'
 
 class App extends Component {
@@ -22,13 +24,20 @@ class App extends Component {
 		})
 	}
 
+	getTodos = () => {
+		const { input } = this.state
+		return Todos()
+	}
+
 	render ({}, { categories, starred, input, selected, done }) {
+		const todos = this.getTodos()
+
 		return (
 			<div class='mx-auto p-4'>
 				<Nav/>
-				<div class='max-w-screen-lg mx-auto mt-20 space-y-10'>
+				<div class='max-w-screen-lg mx-auto mt-20'>
 					<Finder categories={categories} starred={starred} input={input} selected={selected} done={done} update={this.update} />
-					<List/>
+					<List todos={todos}/>
 				</div>
 			</div>
 		)
