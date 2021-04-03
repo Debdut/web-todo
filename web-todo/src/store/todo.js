92cf83af01160f1044c478457a8ea821fc326016
Debdut Karmakar <iawaiponly@gmail.com>
2021-04-04 00:47:18 +0530

    remove fuse + todos

--- web-todo/src/store/todo.js
+++ web-todo/src/store/todo.js
@@ -1,8 +1,7 @@
-import Fuse from 'fuse.js'
-
 const todos = [
   { 
     id: 1,
+    category: 'Finance',
     title: 'File Bankruptcy at Upsolve',
     link: 'https://upsolve.org',
     selector: '',
@@ -12,8 +11,9 @@ const todos = [
   },
   { 
     id: 2,
+    category: 'Jobs',
     title: 'Apply Job @usefluent',
-    link: 'https://www.notion.so/Founding-Engineer-480e95c5a99649f4b0e0210eeb8c6600',
+    link: 'https://notion.so/Founding-Engineer-480e95c5a99649f4b0e0210eeb8c6600',
     selector: '',
     text: '',
     done: false,
@@ -21,8 +21,9 @@ const todos = [
   },
   { 
     id: 3,
+    category: 'Build',
     title: 'Finish the Devtools Pro Prototype design',
-    link: 'https://www.dropbox.com/debdut/devtools-pro',
+    link: 'https://dropbox.com/debdut/devtools-pro',
     selector: '',
     text: '',
     done: false,
@@ -30,6 +31,7 @@ const todos = [
   },
   { 
     id: 4,
+    category: 'Contact',
     title: 'Approach Pieter Levels for Devtools Pro @levelsio',
     link: 'https://twitter.com/levelsio',
     selector: '',
@@ -37,10 +39,38 @@ const todos = [
     done: false,
     createdAt: '',
   },
+  { 
+    id: 5,
+    category: 'Movies',
+    title: 'Watch Jobs Stanford Commencement Speech 2015',
+    link: 'https://youtube.com/watch?v=UF8uR6Z6KLc',
+    selector: '',
+    text: "Drawing from some of the most pivotal points in his life, Steve Jobs, chief executive officer and co-founder of Apple Computer and of Pixar Animation Studios, urged graduates to pursue their dreams and see the opportunities in life's setbacks -- including death itself -- at the university's 114th Commencement on June 12, 2005.",
+    done: false,
+    createdAt: '',
+  },
+  { 
+    id: 6,
+    category: 'Build',
+    title: 'Build Filter Table Feature like this',
+    link: 'https://tryindex.com/beta',
+    selector: 'body > div:nth-child(7) > div > div > div.content-width-small > h3 > strong',
+    text: 'Filter & Highlight',
+    done: false,
+    createdAt: '',
+  },
+  { 
+    id: 7,
+    category: '',
+    title: 'Build Waitlist on blogletter app',
+    link: 'https://getwaitlist.com',
+    selector: '#__next > div > div:nth-child(2) > div.HowTo___StyledDiv-sc-12fy97t-0.cCxvIF > div:nth-child(1) > div.HowTo___StyledDiv3-sc-12fy97t-2.YQuwC',
+    text: 'Growth and Signups Users can sign up for your product and share their link wihttps://th friends. Reward them for successful referrals.',
+    done: false,
+    createdAt: '',
+  },
 ]
 
-const fuse = new Fuse (todos, {
-  keys: [ 'title', 'link', 'text' ]
-})
-
-export default fuse
+export default function Todos () {
+  return todos
+}
