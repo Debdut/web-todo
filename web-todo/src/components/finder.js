c2d394427890d85147f83235b216e4e438b54db3
Debdut Karmakar <iawaiponly@gmail.com>
2021-04-03 18:57:44 +0530

    finder working

--- web-todo/src/components/finder.js
+++ web-todo/src/components/finder.js
@@ -1,3 +1,33 @@
-export default function Finder ({ update, value }) {
-  return <input class='w-full border rounded-full p-2 px-4 text-xl' type='text' placeholder='Search' value={value} onKeyPress={update} />
+export default function Finder ({ categories = [], selected, starred, input, update }) {
+
+  return (
+    <div>
+      <p class='text-8xl my-10'>📝</p>
+      <h1 class='text-4xl font-bold inline'> Web Todo </h1>
+      <span class='text-4xl hidden md:inline'>|</span>
+      <h2 class='text-xl hidden md:inline'> Create Todo's on Webpages <a class='text-blue-400' href=''>See How →</a></h2>
+
+      <div class='float-right space-x-2 md:space-x-4'>
+        <Star starred={starred} update={e => update(!starred, 'starred')} />
+        <Category categories={categories} update={e => update(e.target.value, 'selected')} />
+      </div>
+
+      <input class='my-10 w-full border rounded-full p-2 px-4 text-xl' type='text' placeholder='Search' value={input} onKeyPress={e => update(e.target.value, 'input')} />
+    </div>
+  )
+}
+
+function Category ({ categories, selected, update }) {
+
+  return (
+    <select class='p-2 pr-10 border-2 border-blue-400' onChange={update} value={selected}>
+      <option default>Category</option>
+      {categories.map((category, index) => <option value={index} key={index}>{category}</option>)}
+    </select>
+  )
+}
+
+function Star ({ starred, update }) {
+  const star = starred ? '⭐' : '✩'
+  return <span class='text-2xl cursor-pointer' onClick={update}>{star}</span>
 }
--- web-todo/src/index.js
+++ web-todo/src/index.js
@@ -1,21 +1,37 @@
+import { Component } from 'preact'
+
 import Nav from './components/nav'
 import Finder from './components/finder'
 import List from './components/list'
 
 import './style'
 
-export default function App() {
-	return (
-		<div class='mx-auto p-4'>
-			<Nav/>
-			<div class='max-w-screen-lg mx-auto mt-20 space-y-10'>
-				<p class='text-8xl mb-10'>📝</p>
-				<h1 class='text-4xl font-bold inline'> Web Todo </h1>
-				<span class='text-4xl'>|</span>
-				<h2 class='text-xl inline'> Create Todo's on Webpages <a class='text-blue-400' href=''>See How →</a></h2>
-				<Finder/>
-				<List/>
+class App extends Component {
+	state = {
+		categories: [ 'Movie', 'Job' , 'Reading' ],
+		selected: null,
+		starred: false,
+		input: ''
+	}
+
+	update = (value, key) => {
+		this.setState(state => {
+			state[key] = value
+			return state
+		})
+	}
+
+	render ({}, { categories, starred, input, selected }) {
+		return (
+			<div class='mx-auto p-4'>
+				<Nav/>
+				<div class='max-w-screen-lg mx-auto mt-20 space-y-10'>
+					<Finder categories={categories} starred={starred} input={input} selected={selected} update={this.update} />
+					<List/>
+				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
+
+export default App
