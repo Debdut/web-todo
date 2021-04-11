b72f8cdc403fd8fe11f3432887340b8519eb041a
Debdut Karmakar <iawaiponly@gmail.com>
2021-04-12 01:54:47 +0530

    db pool

---
+++ server/db/pool.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg')
+
+const { DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT } = require('../config')
+
+const pool = new Pool({
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_DATABASE,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+})
+
+module.export = pool
